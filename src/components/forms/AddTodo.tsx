709a4af0993e987ff298af2e4163cdf8af34141a
Maxime-Bott <bottelberghe.maxime@gmail.com>
2021-12-08 14:30:07 +0100

    WIP: AddTodo

--- src/components/forms/AddTodo.tsx
+++ src/components/forms/AddTodo.tsx
@@ -1,20 +1,28 @@
-import {FC, ReactElement} from "react";
+import {FC, FormEvent, ReactElement} from "react";
 import {Input} from "@chakra-ui/react";
+
 import {useInputs} from "hooks/useInputs";
+import {useTodo} from "hooks/useTodo";
 
 const AddTodo: FC = (): ReactElement => {
     const {inputs, handleChange, reset} = useInputs({todo: ""});
+    const {addTodo} = useTodo([]);
 
-    const handleSubmit = () => {
-        console.log(inputs);
+    const handleSubmit = (e: FormEvent) => {
+        e.preventDefault();
+        addTodo({todo: inputs.todo, isCompleted: false});
         reset();
     };
 
     return (
         <form onSubmit={handleSubmit}>
-            <Input type={"text"} onChange={handleChange} value={inputs.todo}>
-                {"Create new todo"}
-            </Input>
+            <Input
+                type={"text"}
+                onChange={handleChange}
+                name={"todo"}
+                value={inputs.todo}
+                placeholder={"Create a new todo..."}
+            />
         </form>
     );
 };
--- src/hooks/useTodo.tsx
+++ src/hooks/useTodo.tsx
@@ -2,7 +2,6 @@ import {useState} from "react";
 
 type TodoObject = {
     todo: string;
-    id: string;
     isCompleted: boolean;
 };
 
