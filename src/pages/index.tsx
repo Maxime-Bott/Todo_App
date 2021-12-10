a10f088a19e31d4e8caefa649f6ed6a179303dbd
Maxime-Bott <bottelberghe.maxime@gmail.com>
2021-12-10 17:10:37 +0100

    Add background with coloMode

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,5 @@
 import {FC, ReactElement} from "react";
-import {Box} from "@chakra-ui/react";
+import {Box, useColorModeValue} from "@chakra-ui/react";
 
 import {useTodo} from "hooks/useTodo";
 
@@ -7,14 +7,32 @@ import AddTodo from "components/forms/AddTodo";
 import Header from "components/Header";
 import TodoList from "components/TodoList";
 
+import darkHeroBanner from "assets/img/bg-desktop-dark.jpg";
+import lightHeroBanner from "assets/img/bg-desktop-light.jpg";
+
 const Homepage: FC = (): ReactElement => {
     const {todo, addTodo, clearCompletedTodo} = useTodo([]);
+    const bgImage: string = useColorModeValue(lightHeroBanner, darkHeroBanner);
 
     return (
-        <Box>
-            <Header />
-            <AddTodo addTodo={addTodo} />
-            <TodoList todo={todo} clearCompletedTodo={clearCompletedTodo} />
+        <Box
+            bgImage={bgImage}
+            bgSize={"contain"}
+            bgPosition={"top"}
+            backgroundRepeat={"no-repeat"}
+            w={"100vw"}
+            h={"100vh"}
+        >
+            <Box w={"35%"} margin={"auto"} d={"flex"} flexDir={"column"}>
+                <Header />
+                <AddTodo addTodo={addTodo} />
+                {todo.length > 0 && (
+                    <TodoList
+                        todo={todo}
+                        clearCompletedTodo={clearCompletedTodo}
+                    />
+                )}
+            </Box>
         </Box>
     );
 };
