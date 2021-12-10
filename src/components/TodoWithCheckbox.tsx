6e013f1824da33e29d7fb2150d295f3d4e3ce482
Maxime-Bott <bottelberghe.maxime@gmail.com>
2021-12-10 22:41:44 +0100

    WIP: Add TodoWithCheckbox

--- src/components/TodoWithCheckbox.tsx
+++ src/components/TodoWithCheckbox.tsx
@@ -1,15 +1,19 @@
 import {FC, FormEvent, ReactElement} from "react";
-import {Box, Input, Checkbox, Text} from "@chakra-ui/react";
+import {Box, Input, Checkbox, ListItem, Text} from "@chakra-ui/react";
+import {Nullable} from "@types";
 
 interface TodoWithCheckboxProps {
-    type: string;
-    name: string;
-    value: string;
-    placeholder: string;
+    type?: string;
+    name?: string;
+    value?: string;
+    placeholder?: string;
     onChange: (e: FormEvent) => void;
     checkName: string;
     checkValue: any;
     withInput?: boolean;
+    isTodoItem?: boolean;
+    todoText?: string;
+    key?: string;
 }
 
 const TodoWithCheckbox: FC<TodoWithCheckboxProps> = ({
@@ -21,36 +25,48 @@ const TodoWithCheckbox: FC<TodoWithCheckboxProps> = ({
     checkName,
     checkValue,
     withInput = false,
-}): ReactElement => {
-    let $content: ReactElement = <Text />;
+    isTodoItem = true,
+    todoText,
+}): Nullable<ReactElement> => {
+    const $checkbox: ReactElement = (
+        <Checkbox
+            isChecked={checkValue}
+            name={checkName}
+            value={checkValue}
+            onChange={onChange}
+        />
+    );
+
+    let $content: Nullable<ReactElement> = null;
 
     if (withInput) {
         $content = (
-            <Input
-                border={"none"}
-                type={type}
-                onChange={onChange}
-                name={name}
-                value={value}
-                placeholder={placeholder}
-                textDecoration={
-                    checkValue && !withInput ? "line-through" : "none"
-                }
-            />
+            <Box>
+                {$checkbox}
+                <Input
+                    border={"none"}
+                    type={type}
+                    onChange={onChange}
+                    name={name}
+                    value={value}
+                    placeholder={placeholder}
+                />
+            </Box>
         );
     }
 
-    return (
-        <Box>
-            <Checkbox
-                isChecked={checkValue}
-                name={checkName}
-                value={checkValue}
-                onChange={onChange}
-            />
-            {$content}
-        </Box>
-    );
+    if (isTodoItem) {
+        $content = (
+            <ListItem>
+                {$checkbox}
+                <Text textDecoration={checkValue ? "line-through" : "none"}>
+                    {todoText}
+                </Text>
+            </ListItem>
+        );
+    }
+
+    return $content;
 };
 
 export default TodoWithCheckbox;
