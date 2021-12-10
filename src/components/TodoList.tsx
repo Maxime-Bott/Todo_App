f5ea1ccd3dca43a98d66914fdbec1bc541b83999
Maxime-Bott <bottelberghe.maxime@gmail.com>
2021-12-10 22:38:24 +0100

    WIP: TodoList

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,33 +1,31 @@
 import {FC, ReactElement} from "react";
-import {List, ListItem, Box} from "@chakra-ui/react";
-import {Nullable} from "@types";
+import {List, Box} from "@chakra-ui/react";
+
 import {UseTodoProps, TodoObject} from "hooks/useTodo";
+import TodoWithCheckbox from "components/TodoWithCheckbox";
 
 const TodoList: FC<Pick<UseTodoProps, "clearCompletedTodo" | "todo">> = ({
     todo,
     clearCompletedTodo,
-}): Nullable<ReactElement> => {
-    let $content: Nullable<ReactElement> = null;
-
-    if (todo.length > 0) {
-        $content = (
-            <Box>
-                <List d={"flex"} flexDir={"column"}>
-                    {todo.map((t: TodoObject, i: number) => {
-                        return <ListItem key={`todo-${i}`}>{t.todo}</ListItem>;
-                    })}
-                </List>
-                <button
-                    type={"button"}
-                    onClick={() => clearCompletedTodo(todo)}
-                >
-                    {"Clear"}
-                </button>
-            </Box>
-        );
-    }
-
-    return $content;
+}): ReactElement => {
+    return (
+        <Box>
+            <List d={"flex"} flexDir={"column"}>
+                {todo.map((t: TodoObject, i: number) => (
+                    <TodoWithCheckbox
+                        key={`todo-${i}`}
+                        todoText={t.todo}
+                        onChange={() => console.log("TODO: OnChange")}
+                        checkName={"isCompleted"}
+                        checkValue={t.isCompleted}
+                    />
+                ))}
+            </List>
+            <button type={"button"} onClick={() => clearCompletedTodo(todo)}>
+                {"Clear"}
+            </button>
+        </Box>
+    );
 };
 
 export default TodoList;
