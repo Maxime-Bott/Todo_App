de13a438f9eed63af78fc755433b7924c26173e9
Maxime-Bott <bottelberghe.maxime@gmail.com>
2021-12-13 18:49:20 +0100

    Add checkbox hover & bg

--- src/lib/theme.ts
+++ src/lib/theme.ts
@@ -15,7 +15,7 @@ const config: configProps = {
 const styles: {[key: string]: any} = {
     global: (props: any) => ({
         body: {
-            fontSize: "18px",
+            fontSize: "16px",
             bgColor: mode("hsl(0, 0%, 98%)", "hsl(235, 21%, 11%)")(props),
         },
     }),
@@ -27,6 +27,8 @@ const fonts: {[key: string]: string} = {
 };
 
 const colors: {[key: string]: string} = {
+    borderGreyish: "hsl(237, 14%, 26%)",
+    placeholderColor: "hsl(233, 14%, 35%)",
     brightBlue: "hsl(220, 98%, 61%)",
     bgGradient: "linear-gradient hsl(192, 100%, 67%) to hsl(280, 87%, 65%)",
     desaturetBlue: "hsl(235, 24%, 19%)",
@@ -37,7 +39,42 @@ const components: GenericObject = {
     Checkbox: {
         baseStyle: {
             control: {
+                _checked: {
+                    border: "none",
+                    _hover: {
+                        bg: "inerhit",
+                        bgGradient:
+                            "linear(to-br, hsl(192, 100%, 67%), hsl(280, 87%, 65%))",
+                        border: "none",
+                    },
+                    bgGradient:
+                        "linear(to-br, hsl(192, 100%, 67%), hsl(280, 87%, 65%))",
+                },
                 borderRadius: "50%",
+                _hover: {
+                    background:
+                        "linear-gradient(hsl(235, 24%, 19%), hsl(235, 24%, 19%)) padding-box, linear-gradient(to right bottom, hsl(192, 100%, 67%), hsl(280, 87%, 65%)) border-box",
+                    border: " 2px solid transparent",
+                },
+                _focus: {
+                    boxShadow: "none",
+                },
+            },
+            icon: {
+                color: "#fff",
+            },
+        },
+    },
+    Input: {
+        baseStyle: {
+            field: {
+                _focus: {
+                    boxShadox: "none",
+                },
+                _placeholder: {
+                    color: "placeholderColor",
+                },
+                "caret-color": "hsl(220, 98%, 61%)",
             },
         },
     },
