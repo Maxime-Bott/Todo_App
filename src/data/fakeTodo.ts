ebafea4cbf548abee8e26aaad1419a361b3bdf33
Maxime-Bott <bottelberghe.maxime@gmail.com>
2021-12-13 22:03:18 +0100

    Edit data

--- src/data/fakeTodo.ts
+++ src/data/fakeTodo.ts
@@ -3,9 +3,9 @@ import {TodoObject} from "hooks/useTodo";
 export const fakeTodo: TodoObject[] = [
     {todo: "Hello World", isCompleted: false, id: `id-${Date.now()}`},
     {
-        todo: "Thanks for your watching",
+        todo: "Thanks for watching",
         isCompleted: false,
         id: `id-2-${Date.now()}`,
     },
-    {todo: "Good Bye", isCompleted: true, id: `id-3-${Date.now()}`},
+    {todo: "See you 🙋‍♂️", isCompleted: false, id: `id-3-${Date.now()}`},
 ];
